a658b8b5fb67186d5bfd68724e65992c4b643c0e
JadedRain <darkenedninja450@gmail.com>
2024-07-13 10:54:23 -0600

    Created Post model and used changed page to use real data

--- django_blog/blog_app/views.py
+++ django_blog/blog_app/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'JadedRain',
-        'title': 'Post 1',
-        'content': 'First post content',
-        'date_posted': 'Now'
-    },
-        {
-        'author': 'Deserted',
-        'title': 'Post 2',
-        'content': 'Joel a nerd',
-        'date_posted': 'Now'
-    },
-]
+from .models import Post
 
 
 def index(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Posts'
     }
     return render(request, 'blog_app/index.html', context)
