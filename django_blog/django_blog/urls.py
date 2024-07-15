8119e4e393a7597909a3942b5d71647ed22dc738
JadedRain <darkenedninja450@gmail.com>
2024-07-14 21:44:39 -0600

    Created registration page and form

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users_app import views as user_views
 
 urlpatterns = [
     path('', include('blog_app.urls')),
+    path('register/', user_views.register_view, name='register'),
     path('admin/', admin.site.urls),
 ]
