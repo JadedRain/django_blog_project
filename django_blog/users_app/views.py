0303112e3ed9032442b74f76c66365ade4ac7b2f
JadedRain <darkenedninja450@gmail.com>
2024-07-14 22:47:39 -0600

    Updated styling on register view

--- django_blog/users_app/views.py
+++ django_blog/users_app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegistationForm
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account Created: {username}!')
             return redirect('blog-index')
     else:
-        form = UserCreationForm()
-    return render(request, 'users_app/register.html', {'form': form})
+        form = UserRegistationForm()
+    return render(request, 'users_app/register.html', {'title':'Register', 
+                                                       'form': form})
