1c21a5d88ac5d1275578455473d8d7803a7af280
JadedRain <darkenedninja450@gmail.com>
2024-07-16 09:14:44 -0600

    Created login and logout funtionality

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users_app import views as user_views
 
 urlpatterns = [
     path('', include('blog_app.urls')),
     path('register/', user_views.register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users_app/login.html'), name='login'),
+    path('logout/', user_views.user_logout_view, name='logout'),
     path('admin/', admin.site.urls),
 ]
--- django_blog/users_app/views.py
+++ django_blog/users_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import logout
 from .forms import UserRegistationForm
 
 def register_view(request):
@@ -8,9 +9,16 @@ def register_view(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created: {username}!')
+            messages.success(request, 'Your account has been created! You can now log in!')
             return redirect('blog-index')
     else:
         form = UserRegistationForm()
     return render(request, 'users_app/register.html', {'title':'Register', 
                                                        'form': form})
+
+def user_logout_view(request):
+    logout(request)
+    return render(request, 'users_app/logout.html')
+
+def profile(request):
+    return render(request, 'users_app/profile.html')
