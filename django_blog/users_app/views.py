13f94327fd44dc6d9e4b8f8ed34b31bd657e7f08
JadedRain <darkenedninja450@gmail.com>
2024-07-18 16:29:37 -0600

    User can now update profile information

--- django_blog/users_app/views.py
+++ django_blog/users_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistationForm
+from .forms import UserRegistationForm, UserUpdateFrom, ProfileUpdateForm
+
 
 def register_view(request):
     if request.method == 'POST':
@@ -23,4 +24,22 @@ def user_logout_view(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users_app/profile.html')
+    if request.method == 'POST':
+        user_update_form = UserUpdateFrom(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(request.POST, 
+                                                request.FILES, 
+                                                instance=request.user.profile)
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            user_update_form.save()
+            profile_update_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return redirect('profile')
+    else:
+        user_update_form = UserUpdateFrom(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'user_update_form': user_update_form,
+        'profile_update_form': profile_update_form
+    }
+    
+    return render(request, 'users_app/profile.html', context)
